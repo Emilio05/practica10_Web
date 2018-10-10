fd67d831b221c743e2dd88478d4236be0f13490e
Emilio Ferreiras <emilio.rojas05@gmail.com>
2018-10-10 14:39:51 -0400

    subiendo practica 10

---
+++ src/main/java/pucmm/inventarioequipos/InventarioequiposApplication.java
@@ -0,0 +1,14 @@
+package pucmm.inventarioequipos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioequiposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarioequiposApplication.class, args);
+
+        System.out.println("Hola Mundo");
+	}
+}
