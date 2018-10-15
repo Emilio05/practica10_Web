5cde0453c6959e8486801bcce41c6ac1ecfe8bb6
Ricard Rosario <20110714@ce.pucmm.edu.do>
2018-10-15 15:49:30 -0400

    re
    s

--- src/main/java/pucmm/inventarioequipos/controller/AlquilerController.java
+++ src/main/java/pucmm/inventarioequipos/controller/AlquilerController.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import pucmm.inventarioequipos.model.Cliente;
 import pucmm.inventarioequipos.model.ClienteEquipo;
 import pucmm.inventarioequipos.model.Equipo;
 import pucmm.inventarioequipos.service.ClienteEquipoServiceImpl;
+import pucmm.inventarioequipos.service.ClienteServiceImpl;
 import pucmm.inventarioequipos.service.EquipoServiceImpl;
 
 import java.util.ArrayList;
@@ -18,19 +20,32 @@ public class AlquilerController {
     @Autowired
     private ClienteEquipoServiceImpl clienteEquipoService;
 
+    @Autowired
+    private EquipoServiceImpl equipoService;
+
+    @Autowired
+    private ClienteServiceImpl clienteService;
+
     @GetMapping("/alquiler")
     public String alquiler(Model model)
     {
         List<ClienteEquipo> alquileres = new ArrayList<>();
+        List<Equipo> equipos = new ArrayList<>();
+        List<Cliente> clientes = new ArrayList<>();
+        equipos = equipoService.buscarTodosEquipos();
+        clientes = clienteService.buscarTodosClientes();
         alquileres = clienteEquipoService.buscarTodosClientesEquipos();
         model.addAttribute("alquileres", alquileres);
+        model.addAttribute("clientes", clientes);
+        model.addAttribute("equipos", equipos);
+
         return "alquiler";
     }
 
     @PostMapping("/alquiler")
     public String agregarAlquiler(ClienteEquipo alquiler, Model model) {
 
-        model.addAttribute("equipos", alquiler);
+        model.addAttribute("alquiler", alquiler);
         return "alquiler";
     }
     @RequestMapping("/equipo/{id}")
