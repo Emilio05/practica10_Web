470296571c8a69eb3b51d0a802659a9b4eba0704
RichieRosario <RichieRosario@users.noreply.github.com>
2018-10-15 08:38:40 -0400

    pasandome de pc
    lol

---
+++ src/main/java/pucmm/inventarioequipos/controller/AlquilerController.java
@@ -0,0 +1,53 @@
+package pucmm.inventarioequipos.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import pucmm.inventarioequipos.model.ClienteEquipo;
+import pucmm.inventarioequipos.model.Equipo;
+import pucmm.inventarioequipos.service.ClienteEquipoServiceImpl;
+import pucmm.inventarioequipos.service.EquipoServiceImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class AlquilerController {
+
+    @Autowired
+    private ClienteEquipoServiceImpl clienteEquipoService;
+
+    @GetMapping("/alquiler")
+    public String alquiler(Model model)
+    {
+        List<ClienteEquipo> alquileres = new ArrayList<>();
+        alquileres = clienteEquipoService.buscarTodosClientesEquipos();
+        model.addAttribute("alquileres", alquileres);
+        return "alquiler";
+    }
+
+    @PostMapping("/alquiler")
+    public String agregarAlquiler(ClienteEquipo alquiler, Model model) {
+
+        model.addAttribute("equipos", alquiler);
+        return "alquiler";
+    }
+    @RequestMapping("/equipo/{id}")
+    public Equipo obtenerEquipo(@PathVariable int id){
+        return equipoService.buscarPorId(id);
+    }
+
+    @DeleteMapping("/equipo/{id}")
+    public void borrarEquipo(@PathVariable long id) {
+        equipoService.borrarEquipoPorId(id);
+    }
+
+//    @RequestMapping(value = "/cliente", method = RequestMethod.PUT)
+//    public String agregarCliente(@RequestParam int id, @RequestParam String nombre){
+//        Estudiante estudiante = new Estudiante(matricula);
+//        estudiante.setNombre(nombre);
+//        return ""+estudiante.getMatricula();
+//    }
+
+}
---
+++ src/main/java/pucmm/inventarioequipos/controller/UsuariosController.java
@@ -0,0 +1,50 @@
+package pucmm.inventarioequipos.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import pucmm.inventarioequipos.model.Equipo;
+import pucmm.inventarioequipos.service.EquipoServiceImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class UsuariosController {
+
+    @Autowired
+    private EquipoServiceImpl equipoService;
+
+    @GetMapping("/equipos")
+    public String equipos(Model model)
+    {
+        List<Equipo> equipos = new ArrayList<>();
+        equipos = equipoService.buscarTodosEquipos();
+        model.addAttribute("equipos", equipos);
+        return "equipos";
+    }
+
+    @PostMapping("/equipos")
+    public String agregarEquipo(Equipo equipo, Model model) {
+        model.addAttribute("equipos", equipo);
+        return "equipos";
+    }
+    @RequestMapping("/equipo/{id}")
+    public Equipo obtenerEquipo(@PathVariable int id){
+        return equipoService.buscarPorId(id);
+    }
+
+    @DeleteMapping("/equipo/{id}")
+    public void borrarEquipo(@PathVariable long id) {
+        equipoService.borrarEquipoPorId(id);
+    }
+
+//    @RequestMapping(value = "/cliente", method = RequestMethod.PUT)
+//    public String agregarCliente(@RequestParam int id, @RequestParam String nombre){
+//        Estudiante estudiante = new Estudiante(matricula);
+//        estudiante.setNombre(nombre);
+//        return ""+estudiante.getMatricula();
+//    }
+
+}
